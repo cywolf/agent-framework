1cb601817f0672c4f788f6a082a1828d32236ea1
Andrew Whitehead <andrew@1crm.com>
2019-03-09 17:38:06 -0800

    add PWD startup script

---
+++ pwd-startup.sh
@@ -0,0 +1,7 @@
+export AGENT_PORT=9000
+export ETH_CONFIG="eth0"
+export AGENT_NAME="StreetCred Agent"
+export myhost=`ifconfig ${ETH_CONFIG} | grep inet | cut -d':' -f2 | cut -d' ' -f1 | sed 's/\./\-/g'`
+export AGENT_ENDPOINT="http://ip${myhost}-${SESSION_ID}-${AGENT_PORT}.direct.${PWD_HOST_FQDN}"
+docker build -f docker/web-agent.dockerfile -t web_agent .
+docker run -it -p ${AGENT_PORT}:${AGENT_PORT} -e ASPNETCORE_URLS="${AGENT_ENDPOINT}:${AGENT_PORT}" -e ENDPOINT_HOST="${AGENT_ENDPOINT}" -e AGENT_NAME="${AGENT_NAME}" web_agent
